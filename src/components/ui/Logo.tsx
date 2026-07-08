aa15ce7896f522b7d761ad4c76ebbf7e6dac2e55
Midhun M <midhun201819@gmail.com>
2026-07-08 22:50:22 +0530

    ,

---
+++ src/components/ui/Logo.tsx
@@ -0,0 +1,10 @@
+ 
+ export default function Logo(){
+    return    <div className="p-6  flex items-center gap-3">
+                        <div className="w-8 h-8 rounded bg-emerald-400 flex items-center justify-center">
+                            <img src="/logo/logo.png"  alt="FitZelly Logo" className="w-full h-full object-cover rounded-full"/>
+                        </div>
+                        <span className="text-xl font-bold text-white tracking-wider">FITZ<span className="text-emerald-400">ELLY</span></span>
+                    </div>
+ }
+                 
