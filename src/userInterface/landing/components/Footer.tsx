0558da907d557ee6b27aa8d1b9a14fc6a430b682
Midhun M <midhun201819@gmail.com>
2026-07-08 22:39:23 +0530

    adding logo in landing page footer

--- src/userInterface/landing/components/Footer.tsx
+++ src/userInterface/landing/components/Footer.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import Logo from '../../../components/ui/Logo';
 
 export default function Footer() {
     return (
@@ -7,12 +8,7 @@ export default function Footer() {
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
                     <div className="col-span-1 md:col-span-1">
                         <div className="flex items-center gap-2 mb-4">
-                            <div className="w-8 h-8 rounded bg-emerald-400 flex items-center justify-center">
-                                <div className="w-5 h-5 text-black">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><path d="M14.4 14.4 9.6 9.6M18.65 9.35A9 9 0 1 1 5.35 18.65L18.65 9.35M6.34 17.66l-1.41 1.41" /></svg>
-                                </div>
-                            </div>
-                            <span className="text-xl font-bold text-white tracking-wider">FITZ<span className="text-emerald-400">ELLY</span></span>
+                            <Logo/>
                         </div>
                         <p className="text-sm leading-relaxed">
                             Complete gym management software to streamline operations and grow your business.
